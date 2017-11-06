d4ac7e066bcd9af7f636f6adac47458c0e0fe034
Unknown <richie@bzzzt.io>
2017-11-06 15:29:06 +0000

    Add build script

---
+++ Start-BuildOrDeploy.ps1
@@ -0,0 +1,13 @@
+Function Start-BuildOrDeploy{
+    param(
+        [String]$pword
+    )
+$svrConnstring = "SERVER=.;Integrated Security=True;Database=master"
+$InvokeSSDTBoD = Join-Path $PSScriptRoot "\SSDTBoD\InvokeSSDTBoD.ps1"
+. $InvokeSSDTBoD -InstanceUnderUse $svrConnstring -Build -Deploy
+$InvokeSSISBoD = Join-Path $PSScriptRoot "\SSISBoD\InvokeSSISBoD.ps1"
+. $InvokeSSISBoD -InstanceUnderUse $svrConnstring -Build -Deploy
+Deploy SQL Agent Job
+$InvokesaltD = Join-Path $PSScriptRoot "\saltD\InvokesaltD.ps1"
+. $InvokesaltD -InstanceUnderUse $svrConnstring -MachineOrDomainName $env:computername -userName $env:UserName -Password $pword -SQLAgentServerName $env:computername -IntegrationServicesCatalogServer $env:computername
+}
