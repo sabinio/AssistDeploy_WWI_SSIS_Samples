5717edd5c6491b368fbf9dbe690cf80cf4f23052
Unknown <richie@bzzzt.io>
2017-11-06 15:46:03 +0000

    add install for msbuild

--- SSDTBoD/Functions/InvokeMSBuildSSDT.ps1
+++ SSDTBoD/Functions/InvokeMSBuildSSDT.ps1
@@ -6,8 +6,9 @@ function Invoke-MsBuildSSDT {
         
         $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
         if (-not (Test-Path $msbuild)) {
-            Write-Error "No MSBuild installed. Install MSBuild using BuildTools_Full.exe provided."
-            Throw
+            Write-Warning "No MSBuild installed. Install MSBuild using BuildTools_Full.exe provided."
+            Invoke-WebRequest "https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe" -OutFile "$env:TEMP\BuildTools_Full.exe" -UseBasicParsing
+            & "$env:TEMP\BuildTools_Full.exe" /Silent /Full
         }
     $arg1 = "/p:tv=14.0"
     $arg2 = "/p:SSDTPath=$DataToolsFilePath"
