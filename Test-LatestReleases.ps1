61cad73fd553818339de5f79df5b9bcc7e46891c
Unknown <richie@bzzzt.io>
2017-11-08 12:14:33 +0000

    remove prerlease switch

--- Test-LatestReleases.ps1
+++ Test-LatestReleases.ps1
@@ -12,12 +12,12 @@ $svrConnstring = "SERVER=.;Integrated Security=True;Database=master"
 $InvokeSSDTBoD = Join-Path $PSScriptRoot "\SSDTBoD\InvokeSSDTBoD.ps1"
 . $InvokeSSDTBoD -InstanceUnderUse $svrConnstring -Build -Deploy
 $InvokeSSISBoD = Join-Path $PSScriptRoot "\SSISBoD\InvokeSSISBoD.ps1"
-. $InvokeSSISBoD -InstanceUnderUse $svrConnstring -Build -Deploy -IncludePreRelease
+. $InvokeSSISBoD -InstanceUnderUse $svrConnstring -Build -Deploy
 #Deploy SQL Agent Job
 #$env:USERDOMAIN
 #$env:computername
 $InvokesaltD = Join-Path $PSScriptRoot "\saltD\InvokesaltD.ps1"
-. $InvokesaltD -InstanceUnderUse $svrConnstring -MachineOrDomainName $env:computername -userName $env:UserName -Password $pword -SQLAgentServerName $env:computername -IntegrationServicesCatalogServer $env:computername -IncludePreRelease
+. $InvokesaltD -InstanceUnderUse $svrConnstring -MachineOrDomainName $env:computername -userName $env:UserName -Password $pword -SQLAgentServerName $env:computername -IntegrationServicesCatalogServer $env:computername
 }
 
 Test-LatestReleases -pword $env:BuildPassword
